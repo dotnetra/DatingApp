f5d46853828e95ff0216ed46805cb77dc4d7f883
dotnetra <raja0ram786@gmail.com>
2022-08-23 11:59:20 +1000

    End of State 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,26 +1,28 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
             _context = context;
         }
-
+        
         [HttpGet]
+        [AllowAnonymous]
         public async Task <ActionResult<IEnumerable<AppUser>>> GetUser()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize] 
         [HttpGet("{id}")]
         public async Task <ActionResult<AppUser>> GetUser(int id)
         {
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.Text;
 using API.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -24,23 +20,16 @@ namespace API
             _config = config;
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-            
-            });
+            services.AddApplictionServices(_config);
             services.AddControllers();
             services.AddCors();
-           /* services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
-                    .AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            });*/
+            services.addIdentityServices(_config);
+        
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -62,7 +51,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors(x => x.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod());
-          //  app.UseCors();
+            //  app.UseCors();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
